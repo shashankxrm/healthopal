14b1e9e044499a27e11ec431103b3f2fd3f96410
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2024-07-12 15:13:02 +0530

    added files

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,46 @@
-import { Button } from "@/components/ui/button";
+import { PatientForm } from "@/components/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+
+const Home = () => {
+  
+
   return (
-  <div className="flex h-screen max-h-screen">
-    <section className="remove-scrollbar container my-auto">
-      <div className="sub-container max-w-[496px]">
-        <Image
-          src="/assets/icons/logo-full.svg"
-          alt="patient"
-          width={1000}
-          height={1000} 
-          className="mb-12 h-10 w-fit"
-        />
+    <div className="flex h-screen max-h-screen">
 
-      </div>
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
 
-    </section>
-  </div>
-   
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-slate-600 xl:text-left">
+              © 2024 CarePluse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
+    </div>
   );
-}
+};
+
+export default Home;
