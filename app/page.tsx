b0adc3b4e45c15410dfc348909e1320fa24b9348
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2025-01-04 22:18:57 +0530

    changed home pic to healthcare recent img

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ const Home = ({ searchParams }: SearchParamProps) => {
       </section>
 
       <Image
-        src="/assets/images/onboarding-img.png"
+        src="/assets/images/healthcare.jpeg"
         height={1000}
         width={1000}
         alt="patient"
